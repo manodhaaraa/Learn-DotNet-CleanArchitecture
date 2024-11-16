fc2ff014edf965130ac3d1499c495d0515fe7b78
Umesh <umma90dissa@gmail.com>
2024-11-16 12:37:57 +0530

    Update IMS first Step

---
+++ src/IMS/IMS/IMS.UseCases/Inventories/interfaces/IViewInventoriesByNameUseCase.cs
@@ -0,0 +1,9 @@
+﻿using IMS.CoreBussiness;
+
+namespace IMS.UseCases.Inventories.interfaces
+{
+    public interface IViewInventoriesByNameUseCase
+    {
+        Task<IEnumerable<Inventory>> ExecuteAsync(string name = "");
+    }
+}
